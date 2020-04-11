c4e015ea399170d81c3e57b77471f456eb552748
evgeniy stoma <resolvertwo@gmail.com>
2020-04-11 17:17:47 +0300

    update13

--- app.js
+++ app.js
@@ -6,40 +6,77 @@ app.use(express.static('public'));
 app.set('view engine', 'pug');
 app.use(express.json());
 
+const PORT = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
     res.render('index.html');
 });
 
-const PORT = process.env.PORT || 3000;
+app.get('/storys', (req, res) => {
+        let username = req.query.name;
+        let result = {};
+        let stories = [];
+    
+        // new Promise((resolve, reject) => {
+        //     request(`https://api.storiesig.com/stories/${username}`, function (error, response, body) {
+        //         error ? reject(console.log(error)) : resolve(result.storysInfo = JSON.parse(body));
+        //     });
+        // }).then(() => {
+        //     console.log(result.storysInfo)
+        // })
 
-app.post('/getstories', (req, res) => {
-    let username = req.body.name;
-    let result = {};
-    
-    async function getStories() {
-        result.storysInfo = await new Promise((resolve, reject) => {
-            request(`https://api.storiesig.com/stories/${username}`, function (error, response, body) {
-                error ? console.log('Не получилось достать сторис, возможно такого имени нет') : resolve(body);
+        async function getStories() {
+                await new Promise((resolve, reject) => {
+                    request(`https://api.storiesig.com/stories/${username}`, function (error, response, body) {
+                        error ? reject(console.log(error)) : resolve(result.storysInfo = JSON.parse(body));
+                    });
+                });
+                await new Promise((resolve, reject) => {
+                    request(`https://www.instagram.com/${username}/?__a=1`, function (error, response, body) {
+                        error ? reject(console.log(error)) : resolve(result.userInfo = JSON.parse(body));
+                    });
+                });
+                await new Promise((resolve, reject) => {
+                    let storysInfo = result.storysInfo;
+                    if (storysInfo.items.length == 0) {
+                        reject('У пользователя нет историй');
+                    } else {
+                        for(let i = 0; i < storysInfo.items.length; i++) {
+                            if (storysInfo.items[i].video_versions === undefined) {
+                                stories.push(storysInfo.items[i].image_versions2.candidates[0].url);
+                            } else {
+                                stories.push(storysInfo.items[i].video_versions[0].url);
+                            }
+                            if (i + 1 == storysInfo.items.length) {
+                                resolve();
+                            }
+                        };
+                    }
+                });
+                res.render('storys.pug', {
+                    result: result,
+                    stories: stories
+                });
+        };
+
+        getStories().catch(e => {
+            console.log(`Error! ${e}`);
+            res.render('storys.pug', {
+                result: result,
+                stories: stories,
+                notStorys : "У этого пользователя нет историй"
             });
-        });
-        result.userInfo = await new Promise((resolve, reject) => {
-            request(`https://www.instagram.com/${username}/?__a=1`, function (error, response, body) {
-                error ? console.log('Не получилось достать сторис, возможно такого имени нет') : resolve(body);
-            });
-        });
-        res.json(JSON.stringify(result));
-    };
-    getStories();
-    
+        })
 });
+
 
 app.post('/search', (req, res) => {
     let val = req.body.name;
     request(`https://www.instagram.com/web/search/topsearch/?context=blended&query=${val}`, function (error, response, body) {
-        res.json(body);
+        error == null ? res.json(body) : console.log('error');
     });
 });
 
 app.listen(PORT, function() {
-    console.log('node express word on 3000');
+    console.log(`node express word on ${PORT}`);
 });
--- public/js/main.js
+++ public/js/main.js
@@ -4,14 +4,14 @@ let searchWrap = document.querySelector('.searchWrap');
 let modal = document.querySelector('.storys_modal');
 let closeModal = document.querySelector('.storys_modal .fa-times');
 
-// let scene = document.getElementById('scene');
-// let parallaxInstance = new Parallax(scene);
-// parallaxInstance.scalar(12, 12);
-// parallaxInstance.friction(0.02, 0.02)
+let scene = document.getElementById('scene');
+if (scene) {
+    let parallaxInstance = new Parallax(scene);
+    parallaxInstance.scalar(12, 12);
+    parallaxInstance.friction(0.02, 0.02)
+}
 
-closeModal.addEventListener('click', function() {
-    modal.classList.remove('active');
-});
+
 
 // fetch('/userinfo', {
 //     method: "POST",
@@ -28,25 +28,28 @@ closeModal.addEventListener('click', function() {
 
 let timeout;
 
-inputName.addEventListener('keyup', function() {
-    clearInterval(timeout);
-    timeout = setTimeout(function() {
-        let accName = inputName.value;
-        fetch('/search', {
-            method: "POST",
-            body: JSON.stringify({name: accName}),
-            credentials: 'same-origin',
-            headers: {
-                'Content-Type': 'application/json'
-              },
-        }).then((response) => {
-            return response.json();
-        }).then((data) => {
-            renderSearch(data);
-        });
-    } ,500);
-    
-});
+if (inputName) {
+    inputName.addEventListener('keyup', function() {
+        clearInterval(timeout);
+        timeout = setTimeout(function() {
+            let accName = inputName.value;
+            fetch('/search', {
+                method: "POST",
+                body: JSON.stringify({name: accName}),
+                credentials: 'same-origin',
+                headers: {
+                    'Content-Type': 'application/json'
+                  },
+            }).then((response) => {
+                return response.json();
+            }).then((data) => {
+                renderSearch(data);
+            });
+        } ,500);
+        
+    });
+}
+
 
 function renderSearch(data) {
     searchWrap.innerHTML = "";
@@ -86,21 +89,22 @@ function renderSearch(data) {
             inputName.value = name;
 
             if (name !== "") {
-                fetch('/getstories', {
-                    method: "POST",
-                    body: JSON.stringify({name: name}),
-                    credentials: 'same-origin',
-                    headers: {
-                        'Content-Type': 'application/json'
-                      },
-                }).then((response) => {
-                    return response.json();
-                }).then((data) => {
-                    itemsWrap.style.pointerEvents = "all";
-                    preloader.classList.remove('active');
-                    // console.log(JSON.parse(data));
-                    renderStories(JSON.parse(data));
-                });
+                window.location.href = `/storys?name=${name}`;
+                // fetch('/getstories', {
+                //     method: "POST",
+                //     body: JSON.stringify({name: name}),
+                //     credentials: 'same-origin',
+                //     headers: {
+                //         'Content-Type': 'application/json'
+                //       },
+                // }).then((response) => {
+                //     return response.json();
+                // }).then((data) => {
+                //     itemsWrap.style.pointerEvents = "all";
+                //     preloader.classList.remove('active');
+                //     // console.log(JSON.parse(data));
+                //     // renderStories(JSON.parse(data));
+                // });
             };
         });
     });
@@ -111,70 +115,84 @@ function renderSearch(data) {
 
 
 function renderStories(data) {
-    modal.classList.add('active');
+
+    // modal.classList.add('active');
     
-    let storriesWrap = document.querySelector('.storiesWrap');
-    let stories = [];
-    let storysInfo = JSON.parse(data.storysInfo);
-    let userInfo = JSON.parse(data.userInfo);
-    console.log(userInfo);
-    for(let i = 0; i < storysInfo.items.length; i++) {
-        if (storysInfo.items[i].video_versions === undefined) {
-            stories.push(storysInfo.items[i].image_versions2.candidates[0].url);
-        } else {
-            stories.push(storysInfo.items[i].video_versions[0].url);
-        }
-    };
+    // let storriesWrap = document.querySelector('.storiesWrap');
+    
+    // let stories = [];
+    // let storysInfo = JSON.parse(data.storysInfo);
+    // let userInfo = JSON.parse(data.userInfo);
+    // console.log(userInfo);
+    // for(let i = 0; i < storysInfo.items.length; i++) {
+    //     if (storysInfo.items[i].video_versions === undefined) {
+    //         stories.push(storysInfo.items[i].image_versions2.candidates[0].url);
+    //     } else {
+    //         stories.push(storysInfo.items[i].video_versions[0].url);
+    //     }
+    // };
 
-    storriesWrap.innerHTML = "";
-    for(let i = 0; i < stories.length; i++) {
-        if (stories[i].indexOf('jpg') !== -1) {
-            let item = `
-            <div class="imgWrap">
-                <img src="${stories[i]}">
-            </div>
+    // storriesWrap.innerHTML = "";
+    // for(let i = 0; i < stories.length; i++) {
+    //     if (stories[i].indexOf('jpg') !== -1) {
+    //         let item = `
+    //         <div class="imgWrap">
+    //             <div class="resultOverlay">
+    //                 <a href="${stories[i]}" download="insta-tool.png">
+    //                     Скачать
+    //                     <img src="img/download.png">
+    //                 </a>
+    //             </div>
+    //             <img src="${stories[i]}">
+    //         </div>
             
-            `;
-            storriesWrap.innerHTML += item;
-        } 
-        if (stories[i].indexOf('mp4') !== -1) {
-            let item = `
-            <div class="videoWrap">
-                <video src="${stories[i]}" controls></video>
-            </div>
-            `;
-            storriesWrap.innerHTML += item;
-        }
-    };
+    //         `;
+    //         storriesWrap.innerHTML += item;
+    //     } 
+    //     if (stories[i].indexOf('mp4') !== -1) {
+    //         let item = `
+    //         <div class="videoWrap">
+    //             <div class="resultOverlay">
+    //                 <a href="${stories[i]}" download="insta-tool.mp4">
+    //                     Скачать
+    //                     <img src="img/download.png">
+    //                 </a>
+    //             </div>
+    //             <video src="${stories[i]}" controls></video>
+    //         </div>
+    //         `;
+    //         storriesWrap.innerHTML += item;
+    //     }
+    // };
 
-    let infoContainer = document.querySelector('.storys_modal .info');
-    infoContainer.innerHTML = "";
-    let img = userInfo.graphql.user.profile_pic_url;
-    let name = userInfo.graphql.user.username;
-    let fullName = userInfo.graphql.user.full_name;
-    let posts = userInfo.graphql.user.edge_owner_to_timeline_media.count;
-    let podpisok = userInfo.graphql.user.edge_follow.count;
-    let podpischikov = userInfo.graphql.user.edge_followed_by.count;
+    // let infoContainer = document.querySelector('.storys_modal .info');
+    // infoContainer.innerHTML = "";
+    // let img = userInfo.graphql.user.profile_pic_url;
+    // let name = userInfo.graphql.user.username;
+    // let fullName = userInfo.graphql.user.full_name;
+    // let posts = userInfo.graphql.user.edge_owner_to_timeline_media.count;
+    // let podpisok = userInfo.graphql.user.edge_follow.count;
+    // let podpischikov = userInfo.graphql.user.edge_followed_by.count;
 
-    let infoLayout = `
-    <div class="info__img">
-        <img src="${img}" alt="">
-    </div>
-    <div class="info__names">
-        <span class="name">${name}</span>
-        <span class="fullName">${fullName}</span>
-    </div>
-    <div class="info__stats">
-        <span>Публикаций </span>
-        <p>${posts}</p>
-        <span>Подписчиков </span>
-        <p>${podpischikov}</p>
-        <span>Подписок </span>
-        <p>${podpisok}</p>
-    </div>
-    `;
+    // let infoLayout = `
+    // <div class="info__img">
+    //     <img src="${img}" alt="">
+    // </div>
+    // <div class="info__names">
+    //     <span class="name">${name}</span>
+    //     <span class="fullName">${fullName}</span>
+    // </div>
+    // <div class="info__stats">
+    //     <span>Публикаций </span>
+    //     <p>${posts}</p>
+    //     <span>Подписчиков </span>
+    //     <p>${podpischikov}</p>
+    //     <span>Подписок </span>
+    //     <p>${podpisok}</p>
+    // </div>
+    // `;
 
-    infoContainer.innerHTML = infoLayout;
+    // infoContainer.innerHTML = infoLayout;
 };
 
 
